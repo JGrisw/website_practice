f2bc951f54294d537752b949c87b1403409f0ad5
Justin Griswold <theeTGP@gmail.com>
2022-04-21 09:48:46 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { NotesComponent } from './notes/notes.component';
+import { TodosComponent } from './todos/todos.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'bookmarks', component: BookmarksComponent, data: { tabNumber: 1 } },
+  { path: 'todos', component: TodosComponent, data: { tabNumber: 2 }},
+  { path: 'notes', component: NotesComponent, data: { tabNumber: 3 }}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'], 
+  animations: [
+    trigger('routeAnim', [
+      transition(':increment', [
+        style({
+          position:'relative', 
+          overflow:'hidden'
+        }),
+        query(':enter, :leave', [
+          style({
+            position:'absolute',
+            top: 0,
+            left:0, 
+            width:'100%',
+            height:'100%',
+            display:'block',
+          })
+        ], {optional:true}),
+        group([
+          query(':leave', [
+            animate('280ms ease-in', style ({
+              opacity:0, 
+              transform: 'translateX(-80px)'
+            }))
+          ], {optional:true}),
+          query(':enter', [
+            style({
+              transform: 'translateX(80px)',
+              opacity:0
+            }),
+            animate('280ms 120ms ease-out', style ({
+              opacity:1,
+              transform:'translateX(0)'
+            }))
+          ], {optional:true})
+        ])
+      ]),
+
+      transition(':decrement', [
+        style({
+          position:'relative', 
+          overflow:'hidden'
+        }),
+        query(':enter, :leave', [
+          style({
+            position:'absolute',
+            top: 0,
+            left:0, 
+            width:'100%',
+            height:'100%',
+            display:'block',
+          })
+        ], {optional:true}),
+        group([
+          query(':leave', [
+            animate('280ms ease-in', style ({
+              opacity:0, 
+              transform: 'translateX(80px)'
+            }))
+          ], {optional:true}),
+          query(':enter', [
+            style({
+              transform: 'translateX(-80px)',
+              opacity:0
+            }),
+            animate('280ms 120ms ease-out', style ({
+              opacity:1,
+              transform:'translateX(0)'
+            }))
+          ], {optional:true})
+        ])
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'personal-website';
+
+  prepareRoute(outlet: RouterOutlet){
+    if(outlet.isActivated)return outlet.activatedRouteData['tabNumber']
+  }
+  
 }
