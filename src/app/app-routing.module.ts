e872db1af76a15ecdc9f87414a3360506d7083be
Justin Griswold <theeTGP@gmail.com>
2022-06-29 09:44:58 -0500

    updated bookmarks section added create and manage views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { EditBookmarkComponent } from './edit-bookmark/edit-bookmark.component';
+import { AddBookmarkComponent } from './add-bookmark/add-bookmark.component';
 import { NotesComponent } from './notes/notes.component';
 import { TodosComponent } from './todos/todos.component';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
@@ -7,15 +9,20 @@ import { AddNoteComponent } from './add-note/add-note.component';
 import { EditNoteComponent } from './edit-note/edit-note.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { EditTodoComponent } from './edit-todo/edit-todo.component';
+import { ManageBookmarksComponent } from './manage-bookmarks/manage-bookmarks.component';
 
 const routes: Routes = [
   { path:'bookmarks', component: BookmarksComponent, data: { tabNumber: 1 } },
+  { path:'bookmarks/add', component: AddBookmarkComponent},
+  { path: 'bookmarks/manage', component: ManageBookmarksComponent, children: [
+    { path: ':id', component: EditBookmarkComponent },
+  ]},
   { path: 'todos', component: TodosComponent, data: { tabNumber: 2 }},
-  { path: 'todos/add', component: AddTodoComponent},
-  { path: 'todos/:id', component: EditTodoComponent},
+  { path: 'todos/add', component: AddTodoComponent },
+  { path: 'todos/:id', component: EditTodoComponent },
   { path: 'notes', component: NotesComponent, data: { tabNumber: 3 }},
-  { path: 'notes/add', component: AddNoteComponent},
-  { path: 'notes/:id', component: EditNoteComponent},
+  { path: 'notes/add', component: AddNoteComponent },
+  { path: 'notes/:id', component: EditNoteComponent },
 
 ];
 
---
+++ src/app/edit-bookmark/edit-bookmark.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-bookmark',
+  templateUrl: './edit-bookmark.component.html',
+  styleUrls: ['./edit-bookmark.component.scss']
+})
+export class EditBookmarkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onFormSubmit(ngform: NgForm){}
+
+}
